62fe9aac1a8118b8377349aee34857b18b2736b4
chaoyue <chaoyue@chaoyuedeMacBook-Pro.local>
2017-04-19 15:27:05 +0800

    annotation

---
+++ src/main/java/org/chaoyue/spring/ComponentAnnotationTest.java
@@ -0,0 +1,25 @@
+package org.chaoyue.spring;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by chaoyue on 2017/4/19.
+ */
+@Configuration
+public class ComponentAnnotationTest {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.register(ComponentAnnotationTest.class);
+        annotationConfigApplicationContext.refresh();
+        InjectClass injectClass = annotationConfigApplicationContext.getBean(InjectClass.class);
+        injectClass.print();
+
+    }
+    @MyComponent
+    public static class InjectClass{
+        public void print(){
+            System.out.println("hello annotation !");
+        }
+    }
+}
---
+++ src/main/java/org/chaoyue/spring/MyComponent.java
@@ -0,0 +1,16 @@
+package org.chaoyue.spring;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by chaoyue on 2017/4/19.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface MyComponent {
+    String value() default "";
+}
